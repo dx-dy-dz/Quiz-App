efd2ab27efd89c81bc9780b29b9baf2cd9995999
dx-dy-dz <72966656+dx-dy-dz@users.noreply.github.com>
2021-09-09 15:33:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+const mainBox = document.querySelector('.home')
+const qnBox = document.querySelector('.qn-box')
+const resultBox = document.querySelector('.result-box')
+const questionNumber = document.querySelector('.qn-no')
+const questionText = document.querySelector('.qn-text')
+const optionContainer = document.querySelector('.option-container')
+const answersIndicatorContainer = document.querySelector('.ans-indicator')
+
+let questionCounter = 0
+let currentQuestion
+let availableQuestions = []
+let availableOptions = []
+let correctAnswer = 0
+let attemptedQns = 0
+
+function setAvailableQuestions () {
+  const totalQuestion = questions.length
+  for (let i = 0; i < totalQuestion; i++) {
+    availableQuestions.push(questions[i])
+  }
+}
+
+function getNewQuestion () {
+  questionNumber.innerHTML =
+    ' Question ' + (questionCounter + 1) + ' of ' + questions.length
+
+  const questionIndex =
+    availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+  currentQuestion = questionIndex
+  questionText.innerHTML = currentQuestion.question
+
+  const index1 = availableQuestions.indexOf(questionIndex)
+  availableQuestions.splice(index1, 1)
+
+  const optionLen = currentQuestion.options.length
+
+  for (let i = 0; i < optionLen; i++) {
+    availableOptions.push(i)
+  }
+  optionContainer.innerHTML = ''
+  let animationDelay = 0.15
+  for (let i = 0; i < optionLen; i++) {
+    const optionIndex =
+      availableOptions[Math.floor(Math.random() * availableOptions.length)]
+    const index2 = availableOptions.indexOf(optionIndex)
+    availableOptions.splice(index2, 1)
+
+    const option = document.createElement('div')
+    option.innerHTML = currentQuestion.options[optionIndex]
+    option.id = optionIndex
+    option.style.animationDelay = animationDelay + 's'
+    animationDelay = animationDelay + 0.15
+    option.className = 'option'
+    optionContainer.appendChild(option)
+    option.setAttribute('onclick', 'getResult(this)')
+  }
+
+  questionCounter = questionCounter + 1
+}
+function getResult (element) {
+  const id = parseInt(element.id)
+  if (id === currentQuestion.answer) {
+    element.classList.add('correct')
+    correctAnswer++
+    console.log('correct: ' + correctAnswer)
+  } else {
+    element.classList.add('wrong')
+    const optionLen = optionContainer.children.length
+    for (let i = 0; i < optionLen; i++) {
+      if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+        optionContainer.children[i].classList.add('correct')
+      }
+    }
+  }
+  attemptedQns++
+  unclickable()
+}
+function unclickable () {
+  const optionLen = optionContainer.children.length
+  for (i = 0; i < optionLen; i++) {
+    optionContainer.children[i].classList.add('already-answered')
+  }
+}
+function next () {
+  if (questionCounter == questions.length) {
+    console.log('Quiz is over')
+    stopQuiz()
+  } else {
+    getNewQuestion()
+  }
+}
+function stopQuiz () {
+  qnBox.classList.add('hide')
+  resultBox.classList.remove('hide')
+  showResult()
+}
+function showResult () {
+  resultBox.querySelector('.total-qn').innerHTML = questions.length
+  resultBox.querySelector('.atmpt-qn').innerHTML = attemptedQns
+  resultBox.querySelector('.rght-qn').innerHTML = correctAnswer
+  resultBox.querySelector('.wrng-qn').innerHTML = attemptedQns - correctAnswer
+  const percentage = (correctAnswer / questions.length) * 100
+  resultBox.querySelector('.perc-qn').innerHTML = percentage.toFixed(2) + '%'
+  resultBox.querySelector('.total-score').innerHTML =
+    correctAnswer + ' / ' + questions.length
+}
+function answersIndicator () {
+  const totalQuestion = questions.length
+  for (let i = 0; i < totalQuestion; i++) {
+    const indicator = document.createElement('div')
+    answersIndicatorContainer.appendChild(indicator)
+  }
+}
+function reset () {
+  questionCounter = 0
+  correctAnswer = 0
+  attemptedQns = 0
+}
+function tryAgain () {
+  resultBox.classList.add('hide')
+  qnBox.classList.remove('hide')
+  reset()
+  startQuiz()
+}
+function goHome () {
+  resultBox.classList.add('hide')
+  mainBox.classList.remove('hide')
+  reset()
+}
+function startQuiz () {
+  mainBox.classList.add('hide')
+  qnBox.classList.remove('hide')
+  setAvailableQuestions()
+  getNewQuestion()
+  answersIndicator()
+}
---
+++ questions.js
@@ -0,0 +1,64 @@
+
+const questions = [
+    
+
+    {
+        "question": 'Which of the following is not considered a JavaScript operator?',
+        "options":  ['new','this','delete','typeof'],
+        "answer": 1
+    },
+
+    {
+        "question": 'How do you write "Hello World" in an alert box?', 
+        "options":  ['alertBox(“Hello World”);','msg(“Hello World”);','msgBox(“Hello World”);','alert(“Hello World”);'],
+        "answer": 3
+    },
+
+    {
+        "question": 'How do you create a function in JavaScript?',
+        "options":  ['function myFunction()','function :myFunction()','Function =  myFunction()','def myFunction()'],
+        "answer": 0
+    },
+
+    {
+        "question": 'Which event occurs when the user clicks on an HTML element?',
+        "options":  ['onmouseclick ','onclick','onchange','onmouseover'],
+        "answer": 1
+    },
+
+    {
+        "question": 'What is the file extension for javascript files?',
+        "options":  ['.javas','.js','.script','.javascript'],
+        "answer": 1
+    },
+
+    {
+    "question": 'What is the output of 33==33.0 ?',
+    "options":  ['True','False','NaN','Undefined'],
+    "answer": 0
+    },
+
+   {
+    "question": 'In the JavaScript, which one of the following is not considered as an error:',
+    "options":  ['Syntax errors','Missing semicolon','Division by zero','Missing brackets'],
+    "answer":  2
+    },
+
+    {
+    "question": 'Which of the following is not a JavaScript Data Types?',
+    "options":  ['Boolean','Undefined','Number','Float'],
+    "answer": 3
+    },
+
+    {
+    "question": 'What was the original name of JavaScript when it discovered?',
+    "options":  ['LiveScript','EScript','Mocha','TypeScript'],
+    "answer": 2
+    },
+
+    {
+    "question": 'Which of the following is not a JavaScript framework or library?',
+    "options":  ['Polymer','Meteor','JQuery','Cassandra'],
+    "answer": 3
+    }  
+];
